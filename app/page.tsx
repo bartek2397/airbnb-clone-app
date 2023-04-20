5ccdb9c40f3a383c147b72474b7fd900f468d26f
bartek2397 <bartosz.wszolek97@gmail.com>
2023-04-20 08:05:37 +0200

    Listing Card completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from './components/EmptyState'
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
+export default async function Home() {
+  const listings = await getListings()
+  const currentUser = await getCurrentUser()
 
-export default function Home() {
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
+
   return (
-    <div>
-      <h1 className="text-rose-500 text-2xl">Hello Airbnb!</h1>
-    </div>
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          <div>
+            {listings.map((listings: any) =>{
+              return (
+                <ListingCard currentUser={currentUser} key={listings.id} data={listings} />
+              )
+            })}
+          </div>
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
